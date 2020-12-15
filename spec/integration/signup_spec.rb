4ccf539df22097ae5e7c76a7e3495b9586b67b6e
kamil <devkamilnaija@gmail.com>
2020-12-15 04:27:20 +0100

    ADDS INTEGRATION TEST FOR CREATING POST

--- spec/integration/signup_spec.rb
+++ spec/integration/signup_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Sign up', type: :feature do
-  let(:user) { User.create!(name: 'Mohammad', email: 'jay@gmail.com', password: 'abc123456789') }
-  let(:friend) { User.create!(name: 'James', email: 'fahad@gmail.com', password: 'abc123456789') }
+  let(:user) { User.create!(name: 'Idis', email: 'j@gmail.com', password: 'abc123456789') }
+  let(:friend) { User.create!(name: 'James', email: 'id@gmail.com', password: 'abc123456789') }
 
   scenario 'create account, login and create a post' do
     visit root_path
     click_link 'Sign up'
 
     # Fill the fields
-    fill_in('user[name]', with: 'Mohammad')
-    fill_in('user[email]', with: 'jay@gmail.com')
+    fill_in('user[name]', with: 'Idris')
+    fill_in('user[email]', with: 'j@gmail.com')
     fill_in('user[password]', with: 'abc123456789')
     fill_in('user[password_confirmation]', with: 'abc123456789')
     click_button 'Sign up'
