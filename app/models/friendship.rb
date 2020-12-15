726274c3de9f81f06c5d7a3b530ed16da47c3643
kamil <devkamilnaija@gmail.com>
2020-12-15 04:06:57 +0100

    ADDS UNIT TESTS FOR LIKE<POST AND FRIENDSHIP

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,6 +1,8 @@
 class Friendship < ApplicationRecord
   belongs_to :sent_to, class_name: 'User', foreign_key: 'sent_to_id'
   belongs_to :sent_by, class_name: 'User', foreign_key: 'sent_by_id'
+  validates :sent_by, presence: true
+  validates :sent_to, presence: true
   scope :friends, -> { where('status =?', true) }
   scope :not_friends, -> { where('status =?', false) }
 end
